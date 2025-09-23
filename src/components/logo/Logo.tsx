af07b06837a825bbd5c5b380c303756d7bb8f134
Уразов Андрей <regandess@gmail.com>
2025-09-23 22:46:12 +0300

    hover for Logo

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -6,14 +6,21 @@ import {Link} from "react-router-dom";
 
 export const Logo = () => {
     return (
-        <Link to={'/'}>
+        <LogoLink to={'/'}>
             <FlexWrapper $align='center' $gap='9px'>
                 <LogoIcon iconId={'logo'} width={'16px'} height={'16px'} viewBox={'0 0 16 16'} />
                 <LogoSpan>Andrey</LogoSpan>
             </FlexWrapper>
-        </Link>
+        </LogoLink>
     );
 };
+
+const LogoLink = styled(Link)`
+    transition: filter 0.3s ease, fill 0.3s ease;
+    &:hover {
+        filter: drop-shadow(0 0 6px ${(props) => props.theme.colors.secondary});
+    }
+`;
 
 const LogoSpan = styled.span`
     display: inline-block;
@@ -26,10 +33,6 @@ const LogoSpan = styled.span`
 
 const LogoIcon = styled(Icon)`
     color: ${({ theme }) => theme.colors.primary};
-
-    &:hover {
-        color: ${({ theme }) => theme.colors.secondary};
-    }
 `;
 
 
